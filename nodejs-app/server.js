9089972241f975c177ff0f896fe05178d31da1af
Bharath A <bharatha@sahaj.ai>
2023-08-18 15:30:34 +0530

    Adding workshop files

---
+++ nodejs-app/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+let userInput = 'Learn Docker!';
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <link rel="stylesheet" href="styles.css">
+      </head>
+      <body>
+        <section>
+          <h2>Latest Input</h2>
+          <h3>${userInput}</h3>
+        </section>
+        <form action="/store-goal" method="POST">
+          <div class="form-control">
+            <label>Course Goal</label>
+            <input type="text" name="goal">
+          </div>
+          <button>Set Course Goal</button>
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+app.post('/store-goal', (req, res) => {
+  const newInput = req.body.goal;
+  console.log(newInput);
+  userInput = newInput;
+  res.redirect('/');
+});
+
+app.listen(80);
